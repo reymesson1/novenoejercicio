bd4c000af60fcad7a185a6c30db87ba6ac8b0e89
reymesson1 <rey.messon@verizon.com>
2018-02-08 12:17:01 -0400

    master

--- mydiary41/app/controllers/articles_controller.rb
+++ mydiary41/app/controllers/articles_controller.rb
@@ -1,5 +1,26 @@
 class ArticlesController < ApplicationController
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def index
     @articles = Article.all
   end
@@ -14,8 +35,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
